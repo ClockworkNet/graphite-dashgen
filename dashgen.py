1b436c41409bfbf08fa8544d60a77b5250e54430
Tim Zehta <tim@clockwork.net>
2013-05-13 11:34:02 -0500

    updated to use argparse. improved pep8 compliance.
    - PROFILE positional is now a flag with a default

--- dashgen.py
+++ dashgen.py
@@ -1,14 +1,18 @@
-#!/usr/bin/python
-
+#!/usr/bin/env python
+"""Automates the creation of host dashboards based on existing collectd
+metrics.
+"""
+# Standard library
+import argparse
 from datetime import date
 import glob
 import json
 import logging
 import logging.handlers
-from optparse import OptionParser
 import os
 import sys
 import urllib
+# Third-party
 import yaml
 
 """USAGE: %prog [options] PROFILE HOST [HOST...]
@@ -25,27 +29,25 @@ log.addHandler(screenhdlr)
 
 
 def dash_create(host, host_path, profile):
-    """Create dashboard for specified host and dashboard profile
-    """
+    """Create dashboard for specified host and dashboard profile."""
     defaults = dashconf['defaults']
     today = date.today().strftime('%FT%T')
     dash_name = "%s_%s" % (host, profile)
     log.info("Dashboard: %s" % dash_name)
 
     # dashboard
-    dash = {
-            'name': dash_name,
+    dash = {'name': dash_name,
             'defaultGraphParams': {
                 'width': defaults['width'],
                 'height': defaults['height'],
                 'from': '-%s%s' % (defaults['quantity'], defaults['units']),
                 'until': defaults['until'],
                 'format': defaults['format'],
-                },
+            },
             'refreshConfig': {
                 'interval': defaults['interval'],
                 'enabled': defaults['enabled'],
-                },
+            },
             'graphs': list(),
             'timeConfig': {
                 'startDate': today,
@@ -62,19 +64,18 @@ def dash_create(host, host_path, profile):
 #                'relativeStartQuantity': defaults['relativeStartQuantity'],
 #                'relativeUntilUnits': defaults['relativeUntilUnits'],
 #                'relativeUntilQuantity': defaults['relativeUntilQuantity'],
-                },
+            },
             'graphSize': {
                 'width': defaults['width'],
                 'height': defaults['height'],
-                },
+            },
             }
     dash['graphs'] = graph_create(host, host_path)
     return dash
 
 
 def graph_create(host, host_path):
-    """Create graph for specified host and dashboard profile
-    """
+    """Create graph for specified host and dashboard profile."""
     graphs = list()
     for name in dash_profile['graphs']:
         log.info("  Graph: %s" % name)
@@ -103,7 +104,7 @@ def graph_create(host, host_path):
                 # Verify metric path
                 if metric_verify:
                     verify_glob = "%s/%s" % (metric_path,
-                            graphdef[name]['glob_verify'])
+                                             graphdef[name]['glob_verify'])
                     del(graph_object['glob_metrics'])
                 else:
                     verify_glob = metric_path
@@ -122,16 +123,15 @@ def graph_create(host, host_path):
                 host == dashconf['carbon_match']):
             graph_object = dict(graphdef[name])
             del graph_object['carbon_match']
-            graph = graph_compile(
-                    dashconf['carbon_server'], name, graph_object, None)
+            graph = graph_compile(dashconf['carbon_server'], name,
+                                  graph_object, None)
             if len(graph) > 0:
                 graphs.append(graph)
     return graphs
 
 
 def graph_compile(host, name, graph_object, metric):
-    """Finish compiling graph
-    """
+    """Finish compiling graph."""
     # Graphs consist of 3 parts
     #   1. graph_targets
     #   2. graph_object
@@ -143,36 +143,30 @@ def graph_compile(host, name, graph_object, metric):
     target_object = list()
     target_pairs = list()
     for template in templates:
-        target = template % {
-                'color_combined': color_combined,
-                'color_free': color_free,
-                'host': host,
-                'metric': metric,
-                }
+        target = template % {'color_combined': color_combined,
+                             'color_free': color_free,
+                             'host': host,
+                             'metric': metric,
+                             }
         target_object.append(target)
         target_pairs.append(('target', target))
     graph_targets = urllib.urlencode(target_pairs)
     # title
     if 'title' in graph_object.keys():
-        graph_object['title'] = graph_object['title'] % {
-                'metric': metric
-                }
+        graph_object['title'] = graph_object['title'] % {'metric': metric}
     else:
         graph_object['title'] = name
     graph_object['title'] = graph_object['title'].replace('-', '.')
     # build graph_render
-    graph_render = "/render?%s&%s" % (
-            urllib.urlencode(graph_object),
-            graph_targets
-            )
+    graph_render = "/render?%s&%s" % (urllib.urlencode(graph_object),
+                                      graph_targets)
     # add target(s) to graph_object
     graph_object['target'] = target_object
     return [graph_targets, graph_object, graph_render]
 
 
 def dash_save(dash):
-    """Save dashboard using Graphite libraries
-    """
+    """Save dashboard using Graphite libraries."""
     # Graphite libraries
     sys.path.append(dashconf['webapp_path'])
     os.environ['DJANGO_SETTINGS_MODULE'] = "graphite.settings"
@@ -189,10 +183,10 @@ def dash_save(dash):
         dashboard.save()
 
 
-def set_log_level(opts):
+def set_log_level(args):
     global log
     """Set the logging level."""
-    loglevel = opts.verbose - opts.quiet
+    loglevel = args.verbose - args.quiet
     if loglevel >= 2:
         log.setLevel(logging.DEBUG)
     elif loglevel == 1:
@@ -204,52 +198,53 @@ def set_log_level(opts):
 
 
 def parser_setup():
-    """Instantiate, configure, and return an OptionParser instance.
-    """
-    p = OptionParser(__doc__)
-    p.add_option("-c", "--config-dir", default='.',
-            help="Configuration directory. Contains YAML configuration files.")
-    p.add_option("-v", "--verbose", action="count", default=1,
-            help="Print copious debugging info.")
-    p.add_option("-q", "--quiet", action="count", default=0,
-            help="Suppress output. -qq to suppress ALL output.")
-    return p
+    """Instantiate, configure, and return an argarse instance."""
+    ap = argparse.ArgumentParser(description=__doc__)
+    ap.add_argument("-c", "--config-dir", default=".",
+                    help="Configuration directory. Contains YAML configuration"
+                         "files.")
+    ap.add_argument("-v", "--verbose", action="count", default=1,
+                    help="Print copious debugging info.")
+    ap.add_argument("-q", "--quiet", action="count", default=0,
+                    help="Suppress output. -qq to suppress ALL output.")
+    ap.add_argument("-p", "--profile", default="all",
+                    help="Dashboard profile to load from dashdef.yml")
+    ap.add_argument(metavar="HOST", nargs="*", dest="host_globs",
+                    help="Host glob.")
+    return ap
 
 
 def main():
     global dashconf, dash_profile, graphdef
     # Command Line Options
-    p = parser_setup()
-    opts, args = p.parse_args()
-    set_log_level(opts)
-    try:
-        profile = args.pop(0)
-    except:
-        log.error("PROFILE must be specified")
+    ap = parser_setup()
+    args = ap.parse_args()
+    set_log_level(args)
 
     # read yaml configuration files
-    dashconf = yaml.safe_load(open('%s/dashconf.yml' % opts.config_dir, 'r'))
-    dashdef = yaml.safe_load(open('%s/dashdef.yml' % opts.config_dir, 'r'))
-    graphdef = yaml.safe_load(open('%s/graphdef.yml' % opts.config_dir, 'r'))
+    dashconf = yaml.safe_load(open('%s/dashconf.yml' % args.config_dir, 'r'))
+    dashdef = yaml.safe_load(open('%s/dashdef.yml' % args.config_dir, 'r'))
+    graphdef = yaml.safe_load(open('%s/graphdef.yml' % args.config_dir, 'r'))
     try:
-        dash_profile = dashdef.pop(profile)
+        dash_profile = dashdef.pop(args.profile)
     except:
-        log.error("Invalid dashboard profile specified: '%s'" % profile)
+        ap.error("Invalid dashboard PROFILE specified: '%s'\n" % args.profile)
 
     # Build hosts list
-    host_parent_glob = "%s/%s" % (
-            dashconf['whisper_path'], dashconf['source_glob'])
-    host_paths = list()
-    for arg in args:
-        for host_path in glob.iglob("%s/%s" % (host_parent_glob, arg)):
+    host_parent_glob = "%s/%s" % (dashconf['whisper_path'],
+                                  dashconf['source_glob'])
+    host_paths = set()
+    for host_glob in args.host_globs:
+        for host_path in glob.iglob("%s/%s" % (host_parent_glob, host_glob)):
             host_path = os.path.abspath(host_path)
             if os.path.isdir(host_path):
-                host_paths.append(host_path)
+                host_paths.add(host_path)
+    host_paths = list(host_paths)
     host_paths.sort()
 
     for host_path in host_paths:
         host = os.path.basename(host_path)
-        dash = dash_create(host, host_path, profile)
+        dash = dash_create(host, host_path, args.profile)
         dash_save(dash)
 
 
